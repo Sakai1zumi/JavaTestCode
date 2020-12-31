629bf5468ae70c6de7bbf3c32ceb18f1a3a87ac9
Sakai1zumi <izumi0527@126.com>
2020-12-31 20:00:46 +0800

    Code 12_31_2020

---
+++ 基础语法/src/com/oop/demo05/Teacher.java
@@ -0,0 +1,5 @@
+package com.oop.demo05;
+
+//Teacher is Person：子类
+public class Teacher extends Person{
+}
---
+++ 基础语法/src/com/oop/demo08/Test.java
@@ -0,0 +1,14 @@
+package com.oop.demo08;
+
+//静态导入包
+import static java.lang.Math.PI;
+import static java.lang.Math.random;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println(random());
+        System.out.println(PI);
+    }
+}
